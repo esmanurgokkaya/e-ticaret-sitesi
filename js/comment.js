40ae0791ef08cb5ebf354df1939432d7fbd5219b
Elif Barlık <elifbarlikmsi@gmail.com>
2024-06-09 17:24:00 +0300

    yorum

--- js/comment.js
+++ js/comment.js
@@ -1,31 +1,27 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const btn = document.querySelector(".comments .comment-header .btn-add button");
+    const post = document.querySelector(".comments .comment-body .add-comment .btn");
+    const addComment = document.querySelector(".comments .comment-body .add-comment");
 
-const btn = document.querySelector(".comments .comment-header .btn-add");
-const post = document.querySelector(".comments .comment-body .add-comment .btn");
-const addComment = document.querySelector(".comments .comment-body .add-comment");
+    btn.addEventListener("click", function(e) {
+        if (addComment.classList.contains("hidden")) {
+            addComment.classList.remove("hidden");
+        } else {
+            addComment.classList.add("hidden");
+        }
+    });
 
-btn.addEventListener("click", function(e) {
-    if (addComment.classList.contains("hidden")) {
-        addComment.classList.remove("hidden");
-    } else {
-        addComment.classList.add("hidden");
-    }
-});
+    document.querySelector(".comments .more-comment button").addEventListener("click", function() {
+        var content = document.querySelector(".comments .comment-body");
+        if (content.style.height === "400px") {
+            content.style.height = "auto";
+            this.textContent = "Daha Az yorum Göster";
+        } else {
+            content.style.height = "400px";
+            this.textContent = "Daha Fazla yorum Göster";
+        }
+    });
 
-
-
-document.querySelector(".comments  .more-comment button").addEventListener("click", function() {
-    var content = document.querySelector(".comments .comment-body");
-    if (content.style.height === "400px") {
-        content.style.height = "auto";
-        this.textContent = "Daha Az yorum Göster";
-    } else {
-        content.style.height = "400px";
-        this.textContent = "Daha Fazla yorum Göster";
-    }
-});
-
-
-document.addEventListener('DOMContentLoaded', function() {
     const form = document.querySelector('.comments .comment-body .add-comment form');
     const chatBox = document.querySelector('.comments .comment-body');
 
@@ -53,7 +49,7 @@ document.addEventListener('DOMContentLoaded', function() {
     // Belirli aralıklarla yeni yorumları çekme işlemi
     setInterval(() => {
         const xhr = new XMLHttpRequest();
-        xhr.open("POST", "php/insertcomment.php", true);
+        xhr.open("POST", "php/fetchcomments.php", true);
 
         xhr.onreadystatechange = function() {
             if (xhr.readyState === XMLHttpRequest.DONE) {
@@ -66,5 +62,5 @@ document.addEventListener('DOMContentLoaded', function() {
         };
 
         xhr.send();
-    }, 500);
+    }, 5000);
 });
--- product.php
+++ product.php
@@ -1,8 +1,8 @@
 <?php
 require 'php/config.php' ?>
 <?php
-if(isset( $_SESSION['user_id']))
-$user_id = $_SESSION['user_id'];
+if (isset($_SESSION['user_id']))
+  $user_id = $_SESSION['user_id'];
 ?>
 
 <!DOCTYPE html>
@@ -21,9 +21,9 @@ $user_id = $_SESSION['user_id'];
   <main>
     <section class="main-wrap">
       <?php
-     if (isset($_GET['id']) && filter_var($_GET['id'], FILTER_VALIDATE_INT)) {
-      $urun_id = $_GET['id'];
-    }
+      if (isset($_GET['id']) && filter_var($_GET['id'], FILTER_VALIDATE_INT)) {
+        $urun_id = $_GET['id'];
+      }
 
       $query = "SELECT * FROM urunler  WHERE urun_id = $urun_id ";
       $result = mysqli_query($conn, $query);
@@ -97,7 +97,7 @@ $user_id = $_SESSION['user_id'];
             </div>
           </div>
           <div class="sub-btn">
-          <input type="text" name="urun" value="<?php echo $urun_id; ?>" hidden class="urun_id" />
+            <input type="text" name="urun" value="<?php echo $urun_id; ?>" hidden class="urun_id" />
             <button class="submit">sepete ekle</button>
           </div>
           </form>
@@ -112,8 +112,8 @@ $user_id = $_SESSION['user_id'];
         <h3></h3>
         <h3>yorum sayısı</h3>
         <div class="btn-add">
-        <button >Yorum Ekle</button>
-</div>
+          <button>Yorum Ekle</button>
+        </div>
       </div>
       <div class="comment-body">
         <div class="add-comment hidden">
